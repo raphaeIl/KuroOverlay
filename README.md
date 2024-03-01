01e6f56b02a4385a6195a80a656063b6cc8f7c0a
Michael <michaelsliu3@gmail.com>
2024-02-29 20:16:47 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Kuro Overlay
+《蔚蓝档案》黑白ins总力战安定点提示工具
+
+## 下载
+在 [release 页面](https://github.com/raphaeIl/KuroOverlay/releases) 下载 exe
+
+## 使用
+双击 kuro_overlay.exe
+
+点击 `t` (默认) 即可打开和关闭安定点提示页面
+
+## 配置要求
+* windows pc （目前不支持手机）
+* 16:9 屏幕分辨率 (1920x1080最佳)
+* 蓝叠模拟器（其他的应该也可以）
+* 首先你得能凹到 p2 ～(≧ ∇ ≦)/
+
+## 展示
+页面
+
+![alt text](https://github.com/raphaeIl/KuroOverlay/preview/preview.png)
+
+教程
+
+据我自己测试，忍忍的技能需要放到下图显示的位置才能避免伤害
+
+![alt text](https://github.com/raphaeIl/KuroOverlay/preview/preview2.png)
+
+图因为是我扒的所以不太好 如果有大佬有更精准的图 球球发给我 ～(≧ ω ≦)
+
+## 自定义快捷键
+下载或创建 `config.json` 并放入exe所在文件夹
+
+模板：
+```
+{
+    "toggle_key": "t",
+    "overlay_opacity": 0.5
+}
+```
+
+- `toggle_key`: 开启/关闭 页面快捷键
+- `overlay_opacity`: 页面透明度
+
+## 注释
+目前(好像)只能用于ins难度
+
+[安定点图](https://twitter.com/Midokuni_Mido/status/1575152057438261248) - 感谢 
+[Midokuni](https://twitter.com/Midokuni_Mido) 大佬
+
+作者：[Raphael](https://space.bilibili.com/1270793735) 
+
+脚本未修改任何游戏数据，可以放心使用，有问题请b站私信或开issues
---
+++ kuro_overlay.py
@@ -0,0 +1,93 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap, QPainter, QIcon
+
+from pynput import keyboard
+
+import os
+import sys
+import json
+import threading
+
+class TransparentImageWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        if os.path.exists('./config.json'):
+            config = json.load(open('./config.json', 'r'))
+        else:
+            config = json.load(open(resource_path('config.json'), 'r'))
+
+        self.toggle_key = config["toggle_key"]
+        self.overlay_opacity = config["overlay_opacity"]
+
+        self.opacity = 0
+        self.running = True
+
+        self.initUIs()
+
+    def initUIs(self):
+        self.setWindowTitle("Kuro Overlay")
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.WindowTransparentForInput)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+
+        # Overlay Image
+        self.imageLabel = QLabel(self)
+        self.imageLabel.setGeometry(0, 0, 1920, 1080)
+        self.pixmap = QPixmap(resource_path("kuro_overlay.png"))
+
+        self.updateImageOpacity()
+        self.showFullScreen()
+        
+    def updateImageOpacity(self):
+        tempPixmap = QPixmap(self.pixmap.size())
+        tempPixmap.fill(Qt.transparent)
+
+        painter = QPainter(tempPixmap)
+        painter.setOpacity(self.opacity)
+        painter.drawPixmap(0, 0, self.pixmap)
+        painter.end()
+
+        self.imageLabel.setPixmap(tempPixmap)
+
+    def onKeyPressEvent(self): # Custom pynput
+        def on_press(key):
+            try:
+                if key.char == self.toggle_key:
+                    self.opacity = self.overlay_opacity if self.opacity == 0 else 0
+                    self.updateImageOpacity()
+            except:
+                pass
+            
+            return self.running
+                
+        with keyboard.Listener(on_press=on_press) as listener:
+            listener.join()
+
+    def closeEvent(self, event):
+        self.running = False
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+def start():
+    print("Starting Kuro Overlay...")
+    print("Press 't' to toggle overlay ～(≧ ∇ ≦)/")
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon(resource_path('izuna.ico')))
+
+    overlay = TransparentImageWidget()
+
+    keyListenerThread = threading.Thread(target=overlay.onKeyPressEvent)
+    keyListenerThread.start()
+    
+    app.exec_()
+    print("Exiting...～(≧ ω ≦)")
+    sys.exit()
+
+start()
